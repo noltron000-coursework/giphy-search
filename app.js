3f02eae9f81b74388f2123e96393b5ca12b3c5cf
Nolan Kovacik <nolansmac@Nolans-MacBook-Pro.local>
2018-08-18 10:55:19 -0600

    Completed lesson 1
    Now on lesson 2.

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+
+// app.js
+
+var exphbs  = require('express-handlebars');
 var express = require('express');
 var app = express();
 
-app.listen(3000, function () {
-  console.log('Gif Search listening on port localhost:3000!');
+app.get('/hello-world', function (req, res) {
+  res.send('Hello World!');
 });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
