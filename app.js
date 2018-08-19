c2415b84c12da757607a899e2b377c1881fb90ea
Nolan Kovacik <nolansmac@nolans-mbp.lan>
2018-08-19 11:14:13 -0700

    Section 3 Completed
    Working on Section 4

--- app.js
+++ app.js
@@ -4,29 +4,32 @@ var exphbs  = require('express-handlebars');
 var express = require('express');
 var app = express();
 
-app.get('/hello-world', function (req, res) {
-  res.send('Hello World!');
-});
- 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// app.js
-
-app.get('/hello-gif', function (req, res) {
-  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
-  res.render('hello-gif', {gifUrl: gifUrl})
-})
-
-app.get('/greetings/:name', function (req, res) {
-  var name = req.params.name;
-  res.render('greetings', {name: name});
-})
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
+});
 
 app.get('/', function (req, res) {
-  res.render('home')
-})
+	console.log(req.query)
+	res.render('home')
+});
+
+app.get('/hello-world', function (req, res) {
+	res.send('Hello World!');
+});
+
+app.get('/hello-gif', function (req, res) {
+	var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+	res.render('hello-gif', {gifUrl: gifUrl})
+});
+
+app.get('/greetings/:name', function (req, res) {
+	var name = req.params.name;
+	res.render('greetings', {name: name});
+});
+
+app.get('/?term=:term', function (req, res) {
+	res.render('term')
+});
