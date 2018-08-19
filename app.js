44336d78c3893592edf8e3a22979c9213ea6e78a
Nolan Kovacik <nolansmac@nolans-mbp.lan>
2018-08-19 11:04:21 -0700

    Section 2 Complete
    Working on Section 3

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // app.js
 
 var exphbs  = require('express-handlebars');
@@ -8,10 +7,26 @@ var app = express();
 app.get('/hello-world', function (req, res) {
   res.send('Hello World!');
 });
-
+ 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// app.js
+
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl})
+})
+
+app.get('/greetings/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('greetings', {name: name});
+})
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
